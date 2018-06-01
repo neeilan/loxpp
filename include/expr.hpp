f22d11ea1ac87af4a32b160cda2b144cb69bdc34
Neeilan Selvalingam <neeilan@outlook.com>
2018-05-31 23:47:49 -0700

    Replaced literal value storage based on void pointers

--- include/expr.hpp
+++ include/expr.hpp
@@ -3,46 +3,52 @@
 
 #include "token.hpp"
 
+#include <string>
+
 class Expr {
 };
 
 
 class Binary: public Expr {
 public:
-  Binary(Expr left, Token operator, Expr right);
+  Binary(Expr left, Token operator, Expr right)
+    : left(left), operator(operator), right(right) {}
 
 private:
-    const Expr left;
-    const Token operator;
-    const Expr right;
+  const Expr left;
+  const Token operator;
+  const Expr right;
 };
 
 
 class Grouping: public Expr {
 public:
-  Grouping(Expr expression);
+  Grouping(Expr expression) : expression(expression) {}
 
 private:
-    Expr expression;
+  Expr expression;
 };
 
 
 class Literal: public Expr {
 public:
-  Literal(void *value);
+  Literal(std::string value) : str_value(value) {}
+  Literal(double value) : num_value(value) {}
 
 private:
-    void *value;
+  std::string str_value = NULL;
+  double num_value = NULL;
 };
 
 
 class Unary: public Expr {
 public:
-  Unary(Token operator, Expr right);
+  Unary(Token operator, Expr right)
+    : operator(operator), right(right) {}
 
 private:
-    const Token operator;
-    const Expr right;
+  const Token operator;
+  const Expr right;
 };
 
 #endif
