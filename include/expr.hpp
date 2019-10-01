b7cfd4e85596878f5747010a3990f58721a89453
Neeilan Selvalingam <neeilan@outlook.com>
2019-09-30 22:30:40 -0400

    Use CRTP to replace repeated visitor boilerplate

--- include/expr.hpp
+++ include/expr.hpp
@@ -15,242 +15,130 @@ using std::shared_ptr;
 class Expr
 {
   public:
-    virtual void accept(ExprVisitor<void> *visitor) const = 0;
-    virtual std::string accept(ExprVisitor<std::string> *visitor) const = 0;
-    virtual shared_ptr<InterpreterResult> accept(ExprVisitor<shared_ptr<InterpreterResult>> *visitor) const = 0;
+    virtual void accept(ExprVisitor<void> * visitor) const = 0;
+    virtual std::string accept(ExprVisitor<std::string> * visitor) const = 0;
+    virtual shared_ptr<InterpreterResult> accept(ExprVisitor<shared_ptr<InterpreterResult>> * visitor) const = 0;
+
+
     virtual bool lvalue() const { return false; }
     virtual bool is_object_field() const { return false; }
     virtual bool callable() const { return false; }
     virtual ~Expr(){};
 };
 
-class Binary : public Expr
+
+// Use CRTP (https://en.wikipedia.org/wiki/Curiously_recurring_template_pattern) to avoid
+// having each derived Expr type needing to be aware of potential visitors. We avoid having to
+// repeat the 'accept' methods in each subtype.
+template <typename T>
+class ExprCRTP : public Expr {
+    virtual void accept(ExprVisitor<void> * visitor) const {
+      return visitor->visit(static_cast<const T*>(this));
+    }
+
+    virtual std::string accept(ExprVisitor<std::string> * visitor) const
+    {
+      return visitor->visit(static_cast<const T*>(this));
+    }
+
+    virtual shared_ptr<InterpreterResult> accept(ExprVisitor<shared_ptr<InterpreterResult>> * visitor) const
+    {
+      return visitor->visit(static_cast<const T*>(this));
+    }
+};
+
+class Binary : public ExprCRTP<Binary>
 {
   public:
     Binary(Expr &left, Token op, Expr &right)
         : left(left), op(op), right(right) {}
-
-    virtual void accept(ExprVisitor<void> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual std::string accept(ExprVisitor<std::string> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual shared_ptr<InterpreterResult> accept(ExprVisitor<shared_ptr<InterpreterResult>> *visitor) const
-    {
-        return visitor->visit(this);
-    }
 
     const Expr &left;
     const Token op;
     const Expr &right;
 };
 
-class Grouping : public Expr
+class Grouping : public ExprCRTP<Grouping>
 {
   public:
     explicit Grouping(Expr &expression) : expression(expression) {}
-
-    virtual void accept(ExprVisitor<void> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual std::string accept(ExprVisitor<std::string> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual shared_ptr<InterpreterResult> accept(ExprVisitor<shared_ptr<InterpreterResult>> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
     const Expr &expression;
 };
 
-class StrLiteral : public Expr
+class StrLiteral : public ExprCRTP<StrLiteral>
 {
   public:
     explicit StrLiteral(const std::string &value, bool nil = false) : value(value),
                                                                       nil(nil)
-    {
-    }
-
-    virtual void accept(ExprVisitor<void> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual std::string accept(ExprVisitor<std::string> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual shared_ptr<InterpreterResult> accept(ExprVisitor<shared_ptr<InterpreterResult>> *visitor) const
-    {
-        return visitor->visit(this);
-    }
+    {}
 
     std::string value;
     bool nil;
 };
 
-class NumLiteral : public Expr
+class NumLiteral : public ExprCRTP<NumLiteral>
 {
   public:
     explicit NumLiteral(double value, bool nil = false) : value(value),
                                                           nil(nil)
-    {
-    }
-
-    virtual void accept(ExprVisitor<void> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual std::string accept(ExprVisitor<std::string> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual shared_ptr<InterpreterResult> accept(ExprVisitor<shared_ptr<InterpreterResult>> *visitor) const
-    {
-        return visitor->visit(this);
-    }
+    {}
 
     double value;
     bool nil;
 };
 
-class BoolLiteral : public Expr
+class BoolLiteral : public ExprCRTP<BoolLiteral>
 {
   public:
     explicit BoolLiteral(bool value) : value(value) {}
 
-    virtual void accept(ExprVisitor<void> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual std::string accept(ExprVisitor<std::string> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual shared_ptr<InterpreterResult> accept(ExprVisitor<shared_ptr<InterpreterResult>> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
     bool value;
 };
 
-class Unary : public Expr
+class Unary : public ExprCRTP<Unary>
 {
   public:
     Unary(Token op, Expr &right)
         : op(op), right(right) {}
 
-    virtual void accept(ExprVisitor<void> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual std::string accept(ExprVisitor<std::string> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual shared_ptr<InterpreterResult> accept(ExprVisitor<shared_ptr<InterpreterResult>> *visitor) const
-    {
-        return visitor->visit(this);
-    }
 
     const Token op;
     const Expr &right;
 };
 
-class This : public Expr
+class This : public ExprCRTP<This>
 {
   public:
     This(Token keyword)
         : keyword(keyword) {}
 
-    virtual void accept(ExprVisitor<void> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual std::string accept(ExprVisitor<std::string> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual shared_ptr<InterpreterResult> accept(ExprVisitor<shared_ptr<InterpreterResult>> *visitor) const
-    {
-        return visitor->visit(this);
-    }
 
     const Token keyword;
 };
 
-class Variable : public Expr
+class Variable : public ExprCRTP<Variable>
 {
   public:
     Variable(Token name)
         : name(name) {}
 
-    virtual void accept(ExprVisitor<void> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual std::string accept(ExprVisitor<std::string> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual shared_ptr<InterpreterResult> accept(ExprVisitor<shared_ptr<InterpreterResult>> *visitor) const
-    {
-        return visitor->visit(this);
-    }
 
     virtual bool lvalue() const { return true; }
-
     const Token name;
 };
 
-class Assignment : public Expr
+class Assignment : public ExprCRTP<Assignment>
 {
   public:
     Assignment(Token name, Expr &value)
         : name(name), value(value) {}
 
-    virtual void accept(ExprVisitor<void> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual std::string accept(ExprVisitor<std::string> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual shared_ptr<InterpreterResult> accept(ExprVisitor<shared_ptr<InterpreterResult>> *visitor) const
-    {
-        return visitor->visit(this);
-    }
 
     const Token name;
     const Expr &value;
 };
 
-class Logical : public Expr
+class Logical : public ExprCRTP<Logical>
 {
   public:
     Logical(Expr &left, Token op, Expr &right)
@@ -258,27 +146,13 @@ class Logical : public Expr
           op(op),
           right(right) {}
 
-    virtual void accept(ExprVisitor<void> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual std::string accept(ExprVisitor<std::string> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual shared_ptr<InterpreterResult> accept(ExprVisitor<shared_ptr<InterpreterResult>> *visitor) const
-    {
-        return visitor->visit(this);
-    }
 
     const Expr &left;
     const Token op;
     const Expr &right;
 };
 
-class Call : public Expr
+class Call : public ExprCRTP<Call>
 {
   public:
     Call(Expr &callee, Token paren, std::vector<Expr *> args)
@@ -286,47 +160,19 @@ class Call : public Expr
           paren(paren),
           args(args) {}
 
-    virtual void accept(ExprVisitor<void> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual std::string accept(ExprVisitor<std::string> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual shared_ptr<InterpreterResult> accept(ExprVisitor<shared_ptr<InterpreterResult>> *visitor) const
-    {
-        return visitor->visit(this);
-    }
 
     const Expr &callee;
     const Token paren;
     const std::vector<Expr *> args;
 };
 
-class Get : public Expr
+class Get : public ExprCRTP<Get>
 {
   public:
     Get(Expr &callee, Token name)
         : callee(callee),
           name(name) {}
 
-    virtual void accept(ExprVisitor<void> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual std::string accept(ExprVisitor<std::string> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual shared_ptr<InterpreterResult> accept(ExprVisitor<shared_ptr<InterpreterResult>> *visitor) const
-    {
-        return visitor->visit(this);
-    }
 
     virtual bool is_object_field() const { return true; }
 
@@ -334,7 +180,7 @@ class Get : public Expr
     const Token name;
 };
 
-class Set : public Expr
+class Set : public ExprCRTP<Set>
 {
   public:
     Set(Expr &callee, Token name, Expr &value)
@@ -342,47 +188,19 @@ class Set : public Expr
           name(name),
           value(value) {}
 
-    virtual void accept(ExprVisitor<void> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual std::string accept(ExprVisitor<std::string> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual shared_ptr<InterpreterResult> accept(ExprVisitor<shared_ptr<InterpreterResult>> *visitor) const
-    {
-        return visitor->visit(this);
-    }
 
     Expr &callee;
     const Token name;
     const Expr &value;
 };
 
-class Lambda : public Expr
+class Lambda : public ExprCRTP<Lambda>
 {
   public:
     explicit Lambda(std::vector<Token> parameters, std::vector<Stmt *> body)
         : parameters(parameters),
           body(body) {}
 
-    virtual void accept(ExprVisitor<void> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual std::string accept(ExprVisitor<std::string> *visitor) const
-    {
-        return visitor->visit(this);
-    }
-
-    virtual shared_ptr<InterpreterResult> accept(ExprVisitor<shared_ptr<InterpreterResult>> *visitor) const
-    {
-        return visitor->visit(this);
-    }
 
     const std::vector<Token> parameters;
     const std::vector<Stmt *> body;
